0865c81e9effa479f5fb75c575d3ca4addeda3d2
uvvpavel <paveluvarov@xmos.com>
2026-02-06 14:05:53 +0000

    added test_calc_inv_X_energy

--- tests/lib_aec/aec_unit_tests_new/conftest.py
+++ tests/lib_aec/aec_unit_tests_new/conftest.py
@@ -7,12 +7,35 @@ import py_voice.config.config as pv_config
 import py_voice.core.fdaf_controller as fdc
 import py_voice
 import numpy as np
+import py_vs_c_utils as pvc
 from run_dut import run_dut
 
 PY_VOICE_ROOT = Path(py_voice.__file__).resolve().parent
 default_conf_path = PY_VOICE_ROOT / "config" / "defaults.json"
 default_conf = pv_config.get_config_dict(default_conf_path)
 bin_dir_path = Path(__file__).parent / "bin"
+
+def gen_bfp(rng, len, hr_max, exps=(-31, 31)):
+  data = pvc.rand_int32_arr(rng, len, hr_max)
+  exp = rng.integers(exps[0], exps[1] + 1, size=1, dtype=np.int32)
+  data_fl64 = pvc.int32_to_double(data, exp)
+  data_int = np.concatenate([exp, data])
+  return data_int, data_fl64
+
+def gen_bfps(rng, hr_max, shape, exps=(-31, 31)):
+  bfp_len = shape[-1]
+  bfp_num = 1
+  data_int = np.empty(0, dtype=np.int32)
+  data_fl64 = np.empty(0, dtype=np.float64)
+  for dim in range(len(shape) - 1):
+    bfp_num *= shape[dim]
+
+  for _ in range(bfp_num):
+    bfp_int, arr_fl64 = gen_bfp(rng, bfp_len, hr_max, exps)
+    data_int = np.append(data_int, bfp_int)
+    data_fl64 = np.append(data_fl64, arr_fl64)
+  
+  return data_int, data_fl64
 
 def set_aec_conf(y_ch, x_ch, main_ph, shadow_ph):
     test_conf = default_conf
